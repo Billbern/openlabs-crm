1964c2a5222f0ff110b48906c46c259e4aac305e
nanaseiwaa <nanaseiwaa24@gmail.com>
2022-04-30 12:18:59 +0000

    added Job cards

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,23 @@
+// import express module
 const express = require('express');
 const route = express.Router();
 
-
+// handle login page request
 route.get('/login', (req, res)=>{
     res.render('auth/login')
 })
 
+// handle signup page request
 route.get('/register', (req, res)=>{
     res.render('auth/register')
 })
 
+// handle login request
 route.post('/login', (req, res)=>{
     res.redirect('/company/user')
 })
 
+// handle registration request
 route.post('/register', (req, res)=>{
     res.sendStatus(200);
 })
